088308fc6cdc0a3e3f3553445d8abd50c1104bbc
pushkaraggrawal <57294033+pushkaraggrawal@users.noreply.github.com>
2021-01-29 18:12:37 +0530

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,73 +3,67 @@ module.exports = {
     // Site URL for when it goes live
     siteUrl: `https://elastic-meninsky-aa7c74.netlify.app/`,
     // Your Name
-    name: 'Ryan Fitzgerald',
+    name: 'Pushkar Aggrawal   ',
     // Main Site Title
-    title: `Ryan Fitzgerald | Full-Stack Developer`,
+    title: `Pushkar Aggrawal | Programmer and Machine Learning Researcher`,
     // Description that goes under your name in main bio
-    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit, ipsum.`,
+    description: `Always willing to learn more. Enthusiastic programmer and researcher of Machine Learning from
+Reinforcement Learning to Computer Vision`,
     // Optional: Twitter account handle
-    author: `@rfitzio`,
+    
     // Optional: Github account URL
-    github: `https://github.com/ryanfitzgerald`,
+    github: `https://github.com/pushkaraggrawal`,
     // Optional: LinkedIn account URL
-    linkedin: `https://www.linkedin.com/in/ryanafitzgerald/`,
+    linkedin: `https://www.linkedin.com/in/pushkar-aggrawal-739361175/`,
     // Content of the About Me section
     about: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus perferendis porro cumque ea error ab voluptatem. Temporibus adipisci exercitationem similique itaque quibusdam laudantium, qui molestiae quas, aut amet animi id.`,
     // Optional: List your projects, they must have `name` and `description`. `link` is optional.
     projects: [
       {
-        name: 'Devfolio',
+        name: 'Screening of Chest X-Rays for COVID using AI',
         description:
-          'A zero-config and blazing fast personal site + blog built with GatsbyJs and TailwindCSS',
-        link: 'https://github.com/RyanFitzgerald/devfolio',
+          'Summer Research Intern @ Bhaskaracharya Institute for Space Applications and Geoinformatics (BISAG),Working on a real life problem, with a strong constraint onavailability of healthy data. Screening of Chest X-Rays for COVID using AI. Thorough examination of current datasets to minimize bias, multiple iterations of training of various models in varied hyper-parameters. Applied techniques like data augmentation and semantic segmentation. Testing through CAM analysis and various numerical analysis techniques. Final GUI application using Flask',
       },
       {
-        name: 'ChromeExtensionKit',
+        name: 'Multimodal Biometrics',
         description:
-          'Kit to jump-start your Chrome extension projects with a variety of battle-tested starter templates',
-        link: 'https://chromeextensionkit.com/?ref=devfolio',
+          'Research @ Department of Electrical & Electronics Engineering, BITS Pilani.Working on fusion of multiple modalities such as ear and fingerprints for a robust biometric model using using techniques such as Siamese Networks and Auto-Encoders.
+        ',
       },
       {
-        name: 'Another Cool Project',
+        name: 'Applications of Federated Learning in Healthcare',
         description:
-          'Lorem ipsum dolor sit amet consectetur adipisicing elit ducimus perferendis',
-        link: 'https://github.com/RyanFitzgerald/devfolio',
+          'Research @ Disruptive Technologies Lab, BITS Pilani. Application of cGANs to augment dataset in a setting where data is sparse in features such as medical tests, prescriptions etc. Further application of blockchain based decentralized learning using PySyft and Etherium frameworks. Currently under work.',
       },
     ],
     // Optional: List your experience, they must have `name` and `description`. `link` is optional.
     experience: [
       {
-        name: 'Acme Corp',
-        description: 'Full-Stack Developer, February 2020 - Present',
-        link: 'https://github.com/RyanFitzgerald/devfolio',
-      },
+        name: 'Bhaskaracharya Institute for Space Applications and Geoinformatics (BISAG)',
+        description: 'Summer Research Intern, 05/2020 - 07/2020',
+        },
       {
-        name: 'Globex Corp',
-        description: 'Full-Stack Developer, December 2017 - February 2020',
-        link: 'https://github.com/RyanFitzgerald/devfolio',
+        name: 'Disruptive Technologies Lab, BITS Pilani',
+        description: 'Student Researcher, 01/2021 - Present,',
+        
       },
-      {
-        name: 'Hooli',
-        description: 'Full-Stack Developer, May 2015 - December 2017',
-        link: 'https://github.com/RyanFitzgerald/devfolio',
-      },
+      
     ],
     // Optional: List your skills, they must have `name` and `description`.
     skills: [
       {
-        name: 'Languages & Frameworks',
+        name: 'Languages',
         description:
-          'JavaScript (ES6+), Golang, Node.js, Express.js, React, Ruby on Rails, PHP',
+          'Python, MATLAB, Java, Javasrcipt',
       },
       {
-        name: 'Databases',
-        description: 'MongoDB, PostreSQL, MySQL',
+        name: 'ML Frameworks',
+        description: 'Pytorch, Keras',
       },
       {
         name: 'Other',
         description:
-          'Docker, Amazon Web Services (AWS), CI / CD, Microservices, API design, Agile / Scrum',
+          'Data Structures and Algorithms, Object Orented Programming, Operaing Systems, Effective Communication, Research and Strategy, Teamwork, Soft Skills',
       },
     ],
   },
